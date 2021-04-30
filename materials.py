d64787e1fe47bd887f6691612364f120004e308d
Kaifee Haque <kaifeeahaque@outlook.com>
2021-04-30 17:07:33 -0400

    Add support for multiple lights

--- materials.py
+++ materials.py
@@ -2,7 +2,7 @@ from bodies import *
 
 # colors
 green = Color(ambient = (0, 0.1, 0), diffuse = (0, 0.7, 0), specular = (1, 1, 1))
-orange = Color(ambient = (0.1, 0.05, 0.03), diffuse = (1, 0.5, 0.3), specular = (1, 1, 1))
+grey = Color(ambient = (0.1, 0.1, 0.1), diffuse = (0.5, 0.5, 0.5), specular = (1, 1, 1))
 purple = Color(ambient = (0.07, 0, 0.1), diffuse = (0.7, 0, 1), specular = (1, 1, 1))
 white = Color(ambient = (1, 1, 1), diffuse = (1, 1, 1), specular = (1, 1, 1))
 soft_white = Color(ambient = (1, 0.8, 0.8), diffuse = (1, 0.8, 0.8), specular = (1, 1, 1))
--- scene.py
+++ scene.py
@@ -4,35 +4,43 @@ from raytracer import *
 from matplotlib.pyplot import imsave
 
 # screen setup
-width, height = 150, 100
+width, height = 1920, 1080
 aspect_ratio = width / height
 screen = {
     "left": -1,
     "top": 1 / aspect_ratio,
     "right": 1,
     "bottom": -1 / aspect_ratio,
-    "z": 3
+    "z": 1.25
 }
 
 # scene setup
-camera = Body([0, 0, screen["z"] + 1])
-light = Light((-4, 10, 3), soft_white)
+focal_length = 1
+reflection_depth = 3
+
+camera = Body([0, 0, screen["z"] + focal_length])
+lights = [
+    Light((-4, 10, 3), white),
+    Light((4, 2, 3), white),
+]
 objects = [
     Sphere((-0.5, 0.1, 0.5), 0.17, purple, shiny),
     Sphere((0, 0.01, -1), 0.75, cyan, shiny),
     Sphere((0.3, 0.01, 0.35), 0.2, green, shiny),
     Sphere((-0.1, 0.03, 0.45), 0.07, white, matte),
-    Plane((0.3, -1, -0.2), orange, shiny)
+    Plane((0.3, -1, -0.2), grey, shiny)
 ]
-reflection_depth = 3
 
 image = np.zeros((height, width, 3))
 
 for i, y in enumerate(np.linspace(screen["top"], screen["bottom"], height)):
     for j, x in enumerate(np.linspace(screen["left"], screen["right"], width)):
-        color = raytrace(x, y, camera, screen["z"], reflection_depth, objects, light)
+        color = np.zeros((3))
+        for light in lights:
+            color += raytrace(x, y, camera, screen["z"], reflection_depth, objects, light)
+        color = color / len(lights)
         image[i, j] = np.clip(color, 0, 1)
 
     print(f"Progress: {round(i * 100 / height)}%")
 
-imsave('scene.png', image)
+imsave('output/scene.png', image)
